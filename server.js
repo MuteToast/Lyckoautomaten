978e483c07f433bf9cac6fa52ea795f65c20d544
MuteToast <theoristo7@gmail.com>
2026-01-21 13:37:46 +0100

    2.2.0
    la till så att man kan skriva in sina egna min och max.

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.use(express.static('public'));
 app.get('/api/lucky', (req, res) => {
   const min = 1;
   const max = 100;
+
+    if (req.query.min) {
+        const parsedMin = parseInt(req.query.min, 10);
+        if (!isNaN(parsedMin)) {
+        min = parsedMin;
+        }
+    }
+
+    if (req.query.max) {
+        const parsedMax = parseInt(req.query.max, 10);
+        if (!isNaN(parsedMax)) {
+        max = parsedMax;
+        }
+    }
+
   const number = Math.floor(Math.random() * (max - min + 1)) + min;
 
   res.json({
